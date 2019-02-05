1b840b605d978f63c34411c82d88c39e3b674e11
brumone <dani3544@gmail.com>
2019-02-05 01:21:58 -0200

    funcionalidade1

---
+++ Funcionalidade1/CasaDoCodigo/Migrations/20190205004619_Teste.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Migrations
+{
+    public partial class Teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Cadastro",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Cadastro",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ Funcionalidade1/CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,41 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+	public interface ICategoriaRepository
+	{
+		Task<Categoria> NovaCategoria(string nome);
+	}
+
+	public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+	{
+		public CategoriaRepository(ApplicationContext contexto) : base(contexto)
+		{
+		}
+
+		public async Task<Categoria> NovaCategoria(string nome)
+		{
+			var categoriaDB = dbSet.Where(c => c.NomeCategoria == nome)
+				//Garantindo que não haverão categorias com o mesmo nome
+				.SingleOrDefault();
+
+			if (categoriaDB == null)
+			{
+				var novaCategoria = new Categoria()
+				{
+					NomeCategoria = nome
+				};
+
+				categoriaDB = dbSet.Add(novaCategoria).Entity;
+
+			}
+
+			await contexto.SaveChangesAsync();
+			return categoriaDB;
+		}
+	}
+}
