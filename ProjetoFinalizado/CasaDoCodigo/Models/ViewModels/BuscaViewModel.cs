52f8d17aefc00ae220674113034b98437dacec45
brumone <dani3544@gmail.com>
2019-02-15 20:30:19 -0200

    funcionalidade 3 ok

---
+++ ProjetoFinalizado/CasaDoCodigo/Models/ViewModels/BuscaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+	public class BuscaViewModel
+	{
+		public BuscaViewModel(IList<Produto> produtos, bool encontrouResultados)
+		{
+			this.Produtos = produtos;
+			this.EncontrouResultados = encontrouResultados;
+		}
+
+		public IList<Produto> Produtos { get; set;  }
+
+		//Vai retornar true se encontrou resultados na pesquisa
+		public BuscaViewModel(bool encontrouResultados)
+		{
+			EncontrouResultados = encontrouResultados;
+		}
+
+		public string Pesquisa { get; set; }
+		public bool EncontrouResultados;
+	}
+}
